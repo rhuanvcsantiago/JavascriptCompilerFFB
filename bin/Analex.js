365f85e1ff1cfc296e6044d6031fd4c4c53ce341
Rhuan Victor Crescêncio de Santiago <rhuanvcsantiago@gmail.com>
2017-03-21 03:41:07 -0300

    Refatoração do objeto symbolsTable

--- bin/Analex.js
+++ bin/Analex.js
@@ -1,16 +1,16 @@
 function Analex(){
 
-    this.symbolsTable = []; //Array Tokens
+    this.symbolsTable = new SymbolsTable(); //Array Tokens
 	this.error = [];
     
     this.tokenizer = function(codeFile){
-		this.symbolsTable = []; 
 		this.error = [];
-
-        var token = "";
+		this.symbolsTable.clean();
+        
+		var token = "";
         var classe = "";
 		var classe_anterior = "";   
-        var posicao = { linha:0 , coluna:0 };
+        var posicao = new Position(0,0);
 		
 		for( var i = 1 ; i <= codeFile.rowsCount() ; i++ ) {
 			var row = codeFile.row(i); 
@@ -23,8 +23,7 @@ function Analex(){
 				var char = row.col(j);
 				classe = "";
 				if(token == ""){
-					posicao.linha = i;
-					posicao.coluna = j;
+					posicao = new Position(i,j);
 				}
 				token = token + char;				
 				
@@ -53,7 +52,7 @@ function Analex(){
 				} else if(char == '.') {
 					classe = "ponto";
 				} else {
-					this.error.push(["Caracter Inválido: "+token ,JSON.parse(JSON.stringify(posicao))]);
+					this.error.push(["Caracter Inválido: "+token ,posicao]);
 					classe_anterior = "";
 					token = "";
 					break;
@@ -61,30 +60,30 @@ function Analex(){
 																					
 				if(classe_anterior=="string"){
 					if(classe == "string") {
-						this.symbolsTable.push([token,"string",JSON.parse(JSON.stringify(posicao))]);
+						this.symbolsTable.push(token,"string",posicao);
 						classe_anterior = "";
 						token = "";
 					} else if (classe=="nova_linha") {						
-						this.error.push(["String não fechada",JSON.parse(JSON.stringify(posicao))]);
+						this.error.push(["String não fechada",posicao]);
 						classe_anterior = "";
 						token = "";
 						break;
 					}
 				} else if(classe_anterior=="operacao2") { /* /" /a /1 /  /; /"\n" /* // */
 					if(classe == "char" || classe == "numero" || classe == "espaco") {
-						this.symbolsTable.push([token.substr(0,token.length-1),"divisao",JSON.parse(JSON.stringify(posicao))]);
+						this.symbolsTable.push(token.substr(0,token.length-1),"divisao",posicao);
 						classe_anterior = "";
 						token = "";
 						j--;
 					} else if (classe == "operacao2") {
-						this.symbolsTable.push([token,"comentario",JSON.parse(JSON.stringify(posicao))]);
+						this.symbolsTable.push(token,"comentario",posicao);
 						token = "";						
 						classe_anterior = "";
 						break;
 					} else if (classe == "operacao4") {
 						classe = "comentario_multilinha";
 						if(codeFile.isPositionValid(i,j)) {
-							this.error.push(["Comentario não fechado",JSON.parse(JSON.stringify(posicao))]);
+							this.error.push(["Comentario não fechado",posicao]);
 							token = "";
 							classe_anterior = "";							
 							break;
@@ -105,7 +104,7 @@ function Analex(){
 							var char3 = codeFile.row(i).col(j+1);													
 							var multinhaInvertido = char2+char3;
 							if( multinhaInvertido == "*/" ) {
-								this.symbolsTable.push([token,"comentarioML",JSON.parse(JSON.stringify(posicao))]);
+								this.symbolsTable.push(token,"comentarioML",posicao);
 								token = "";								
 								classe_anterior = "";
 								j++;
@@ -114,13 +113,13 @@ function Analex(){
 							j++;
 						}	
 						if(codeFile.isPositionValid(i,j)) {
-							this.error.push(["Comentario não fechado",JSON.parse(JSON.stringify(posicao))]);							
+							this.error.push(["Comentario não fechado",posicao]);							
 						}
 						if(mudouLinha) {
 							break;
 						}
 					} else {
-						this.error.push(["Lixo sintático: " +token + " (" + classe_anterior + " com " + classe + ")" ,JSON.parse(JSON.stringify(posicao))]);
+						this.error.push(["Lixo sintático: " +token + " (" + classe_anterior + " com " + classe + ")" ,posicao]);
 						token = "";
 						classe_anterior = "";
 						break;
@@ -128,27 +127,27 @@ function Analex(){
 				} else if(classe_anterior=="numero") { 
 					/* CONDICOES QUEBRA: 1+ 1/ 1* 1= 1- 1 1; 1) 1]*/
 					if(classe=="operacao1" || classe == "operacao2" || classe == "operacao3" || classe == "operacao4" || classe == "separador" || classe == "ponto_e_virgula" ||  classe == "espaco")  {
-						this.symbolsTable.push([token.substr(0,token.length-1),"numero",JSON.parse(JSON.stringify(posicao))]);
+						this.symbolsTable.push(token.substr(0,token.length-1),"numero",posicao);
 						classe_anterior = "";
 						token = "";
 						j--;
 					/*NAO PODE: 1a 1'\n' */
 					} else if(classe=="char"){
-						this.error.push(["Má formação de identificador",JSON.parse(JSON.stringify(posicao))]);
+						this.error.push(["Má formação de identificador",posicao]);
 						token = "";
 						classe_anterior = "";
 						break;											
 					} else if (classe == "ponto") {
 						classe_anterior = "ponto_de_numero";
 					} else if(classe != "numero" ) {
-						this.error.push(["Lixo sintático: " +token + " (" + classe_anterior + " com " + classe + ")" ,JSON.parse(JSON.stringify(posicao))]);
+						this.error.push(["Lixo sintático: " +token + " (" + classe_anterior + " com " + classe + ")" ,posicao]);
 						token = "";
 						classe_anterior = "";
 						break;
 					}
 				} else if(classe_anterior=="ponto_de_numero")  {
 					if(classe!="numero") {
-						this.error.push(["Número Invalido: " +token + " (" + classe_anterior + " com " + classe + ")" ,JSON.parse(JSON.stringify(posicao))]);
+						this.error.push(["Número Invalido: " +token + " (" + classe_anterior + " com " + classe + ")" ,posicao]);
 						token = "";
 						classe_anterior = "";
 						break;
@@ -159,33 +158,33 @@ function Analex(){
 					/* QUEBRAS: a<operador> | a<separador> | a<espaco> | a<ponto_e_virgula>
 					   NAO PODE: a<string> | a<ponto> | a<nova_linha>  */
 					   if( /boolean\s|char\s|class\s|double\s|new\s|return\s/i.test(token)) {
-							this.symbolsTable.push([token.substr(0,token.length-1),"palavra_reservada_c_espaco",JSON.parse(JSON.stringify(posicao))]);
+							this.symbolsTable.push(token.substr(0,token.length-1),"palavra_reservada_c_espaco",posicao);
 							classe_anterior = "";
 							token = "";
 							j--;
 							continue;
 					   } else if((classe == 'espaco' || classe == 'ponto_e_virgula') 
 					    			&& /(break|continue|true|false)(;|\s)/i.test(token)){
-							this.symbolsTable.push([token.substr(0,token.length-1),"palavra_reservada",JSON.parse(JSON.stringify(posicao))]);
+							this.symbolsTable.push(token.substr(0,token.length-1),"palavra_reservada",posicao);
 							classe_anterior = "";
 							token = "";
 							j--;
 							continue;
 					   } else if((classe == 'espaco' || classe == 'separador') &&
 					   				/(else|if|while)(\s|\{|\}|\[|\]|\(|\))/i.test(token) ) {
-							this.symbolsTable.push([token.substr(0,token.length-1),"palavra_reservada",JSON.parse(JSON.stringify(posicao))]);
+							this.symbolsTable.push(token.substr(0,token.length-1),"palavra_reservada",posicao);
 							classe_anterior = "";
 							token = "";
 							j--;
 							continue;
 					   }
 					if (classe == "espaco" || classe == "separador" || classe=="operacao1" || classe == "operacao2" || classe == "operacao3" || classe == "operacao4" || classe == "ponto_e_virgula") {
-							this.symbolsTable.push([token.substr(0,token.length-1),"identificador",JSON.parse(JSON.stringify(posicao))]);
+							this.symbolsTable.push(token.substr(0,token.length-1),"identificador",posicao);
 							classe_anterior = "";
 							token = "";
 							j--;
 					} else if(classe == "string" || classe == "nova_linha" || classe == "ponto") {
-						this.error.push(["Lixo sintático: " +token + " (" + classe_anterior + " com " + classe + ")" ,JSON.parse(JSON.stringify(posicao))]);
+						this.error.push(["Lixo sintático: " +token + " (" + classe_anterior + " com " + classe + ")" ,posicao]);
 						token = "";
 						classe_anterior = "";
 						break;
@@ -196,12 +195,12 @@ function Analex(){
 					NAO PODE: +<string> +<operacao4> +<ponto_e_virgula> +<nova_linha> +<ponto>
 					*/
 					if (classe == "char" || classe == "numero" || classe=="espaco" || classe == "separador") {
-						this.symbolsTable.push([token.substr(0,token.length-1),classe_anterior,JSON.parse(JSON.stringify(posicao))]);
+						this.symbolsTable.push(token.substr(0,token.length-1),classe_anterior,posicao);
 						classe_anterior = "";
 						token = "";
 						j--;
 					} else {
-						this.error.push(["Lixo sintático: " +token + " (" + classe_anterior + " com " + classe + ")" ,JSON.parse(JSON.stringify(posicao))]);
+						this.error.push(["Lixo sintático: " +token + " (" + classe_anterior + " com " + classe + ")" ,posicao]);
 						token = "";
 						classe_anterior = "";
 						break;
@@ -213,23 +212,23 @@ function Analex(){
 					NAO PODE:  =<operacao1,2,4> =<ponto_e_virgula> =<nova_linha> =<ponto>
 					*/
 					if (classe == "char" || classe == "numero" || classe=="espaco" || classe == "separador" || classe == "string") {
-						this.symbolsTable.push([token.substr(0,token.length-1),classe_anterior,JSON.parse(JSON.stringify(posicao))]);
+						this.symbolsTable.push(token.substr(0,token.length-1),classe_anterior,posicao);
 						classe_anterior = "";
 						token = "";
 						j--;
 					} else if( classe!="operacao3" ) {
-						this.error.push(["Lixo sintático: " +token + " (" + classe_anterior + " com " + classe + ")" ,JSON.parse(JSON.stringify(posicao))]);
+						this.error.push(["Lixo sintático: " +token + " (" + classe_anterior + " com " + classe + ")" ,posicao]);
 						token = "";
 						classe_anterior = "";
 						break;
 					}
 				} else if(classe_anterior=="separador") {
-					this.symbolsTable.push([token.substr(0,token.length-1),"separador",JSON.parse(JSON.stringify(posicao))]);
+					this.symbolsTable.push(token.substr(0,token.length-1),"separador",posicao);
 					classe_anterior = "";
 					token = "";
 					j--;
 				} else if(classe_anterior=="ponto_e_virgula") {
-					this.symbolsTable.push([token.substr(0,token.length-1),"ponto_e_virgula",JSON.parse(JSON.stringify(posicao))]);
+					this.symbolsTable.push(token.substr(0,token.length-1),"ponto_e_virgula",posicao);
 					classe_anterior = "";
 					token = "";
 					j--;
@@ -238,7 +237,7 @@ function Analex(){
 					token = "";
 					j--;
 				} else if(classe_anterior == "ponto") {
-					this.error.push(["Ponto clandestino: "+token,JSON.parse(JSON.stringify(posicao))]);
+					this.error.push(["Ponto clandestino: "+token,posicao]);
 					token = "";
 					classe_anterior = "";
 					break;
@@ -269,20 +268,20 @@ function Position(row, col){
     this.col = col;
 }
 
-function symbolsTable(row, col){
+function SymbolsTable(){
 	this.array={};
 
-    this.push = function(token, classe, position){
-		
-		if( this.array[token] == undefined )
-			this.array[token] = [];
+    this.push = function(token,classe,position){
+		if (this.array[token] == undefined) 
+			this.array[token] = {};
+
+		if( this.array[token]['classe'] == undefined )
+			this.array[token]['classe'] = classe;
 		
 		if( this.array[token]["positions"] == undefined )
 			this.array[token]["positions"] = [];
 
-		this.array[token].push(class);
 		this.array[token]["positions"].push(position);
-
 	}
 
 	this.clean = function clean(){
