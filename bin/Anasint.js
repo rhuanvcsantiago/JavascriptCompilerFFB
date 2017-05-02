877679aa68e969a334fe70efee94755f263de096
Rhuan Victor Crescêncio de Santiago <rhuanvcsantiago@gmail.com>
2017-05-02 13:58:14 -0300

    asdasd
    asdasd

--- bin/Anasint.js
+++ bin/Anasint.js
@@ -38,32 +38,93 @@ function Anasint(){
 
     // the first nonTerminal of a terminal derivation
     this.first = function(startElement, analysedElement) {
-        var derivations = BNF[startElement].derivations;
-        var firstFounded = false;
+        var derivations = this.BNF[startElement].derivations;
+       
 
-        for(var i = 0; i < derivations.length; i++){
-            // se ainda nao achou o primeiro terminal 
-            // OU se a derivacao é diferente do elemento analisado -> nao entrar em loop
-            if( !firstFounded || (derivations[i] != analysedElement) ){
-                
-                if( isTerminal( derivations[i] ) ) {
-                    this.first( derivations[i], analysedElement );
-                } else {
-                    this.BNF[analysedElement].first.push( derivations[i] );
-                    firstFounded = true;
-                }
-            }    
+        for(var j = 0; j< derivations.length ;j++) {
+            
+            var firstFounded = false;
+            var rule = derivations[j];
+
+            for(var i = 0; i < rule.length; i++){
+                // se ainda nao achou o primeiro terminal 
+                // OU se a derivacao é diferente do elemento analisado -> nao entrar em loop
+                if( !firstFounded && (rule[i] != analysedElement) && (startElement != rule[i]) ){
+                    
+                    if( this.isTerminal( rule[i] ) ) {
+                        firstFounded = this.first( rule[i], analysedElement );
+                    } else {
+                        var alreadyInSet = this.BNF[analysedElement].first.indexOf( rule[i] );
+                        
+                        if(alreadyInSet == -1)
+                            this.BNF[analysedElement].first.push( rule[i] );
+
+                        firstFounded = true;
+                    }
+                }    
+            }
         }
+
+        return firstFounded;
     }
 
     // the first nonTerminal of a brother-terminal derivation
-    this.follow = function(startElement){
-  
+    this.follow = function(element){
+        //pegar o index of de cada regra
+        var firstsArray = [];
+        
+        var leftTerms = Object.keys(this.BNF)
+       
+        for( var k=0; k< leftTerms.length; k++){
+
+            var leftTermIndex = leftTerms[k];
+            var derivations = this.BNF[leftTermIndex].derivations;
+
+            for( var i = 0; i < derivations.length; i++ ){
+
+                var rule = derivations[i];
+                var index = rule.indexOf(element);
+            
+                if( index != -1 ) {
+                    var nextElementIndex = index+1;
+                    var nextElement = "";
+                    
+                    if( nextElementIndex >= rule.length )
+                        nextElement = "ε";
+                    else
+                        nextElement = rule[nextElementIndex];
+
+                    if( firstsArray.indexOf( nextElement ) == -1 )
+                        firstsArray.push( nextElement );  
+                }       
+            } 
+        }
+
+        for( var i = 0; i<firstsArray.length; i++ ){
+            var term = firstsArray[i];
+
+            if( this.isTerminal(term) ) {
+                this.BNF[element].follow = this.BNF[element].follow.concat( this.BNF[term].first );               
+            } else{
+                this.BNF[element].follow =  this.BNF[element].follow.concat( term );  
+            }
+        }
+
+        //this.BNF[element].follow = firstsArray;    
     }
 
-    this.firstAndFollow = function(tokenCode, startElement){
-        this.first (startElement);
-        this.follow(startElement);
+    this.generateFirstAndFollow = function(){
+        
+        var leftTerms = Object.keys(this.BNF);
+
+        for(var i=0; i< leftTerms.length; i++) {
+            this.first( leftTerms[i], leftTerms[i] );            
+        };
+
+        for(var i=0; i< leftTerms.length; i++) {
+            this.follow( leftTerms[i] );            
+        };
+   
     }
 
     this.isTerminal = function(element) {
@@ -73,8 +134,6 @@ function Anasint(){
 
         return false;
     }
-
-
 
 }
 
