acfe4ce96139e969d3e83186cae4948186ed5be2
Rhuan Victor Crescêncio de Santiago <rhuanvcsantiago@gmail.com>
2017-05-04 04:20:31 -0300

    VERSAO FINAL RHUAN - ANASINT
    - BRUTE FORCE FUNCIONANDO
    - 4 EXEMPLOS FUNCIONANDO
    
    - FALTA SÓ FAZER BNF PARA GRAMATICA DO PAULO

--- bin/Compiler.js
+++ bin/Compiler.js
@@ -2,7 +2,7 @@ function Compiler(){
 
     this.codeFile  = new CodeFile(); //nova modelagem -> pode ser um array de arquivos -> abre possibilidade
     this.anaLex    = new Analex();
-    this.anaSint   = new FirstFollow();
+    this.anaSint   = new Anasint();
     
 
     // Function witch runs evetime a Key is pressed.
